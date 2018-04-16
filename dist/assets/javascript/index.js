5e95de7c5c3258424ce5cf827148a50cd4d57412
Michael Mckeever <mckeever02@gmail.com>
2018-04-16 15:46:11 +0100

    Add JS to dist

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -14,4 +14,40 @@ var options = {
 
 var spacesList = new List('help', options);
 
+// Show an element
+var show = function (elem) {
+	elem.classList.add('is-visible');
+};
+
+// Hide an element
+var hide = function (elem) {
+	elem.classList.remove('is-visible');
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+	elem.classList.toggle('is-visible');
+};
+
+var showForm = document.getElementById('show-form');
+var showThanks = document.getElementById('show-thanks');
+var form = document.getElementById('feedback-form');
+var feedback = document.getElementById('feedback');
+var formLabel = document.getElementById('form-label');
+var thanks = document.getElementById('thanks');
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  showForm.addEventListener('click', function() {
+    form.classList.remove('dn');
+    feedback.classList.add('dn');
+    formLabel.innerHTML = "Oh no! How can we improve this?"
+  });
+  showThanks.addEventListener('click', function() {
+    feedback.classList.add('dn');
+    formLabel.innerHTML = "Thanks for the feedback!"
+    thanks.classList.remove('dn');
+  });
+});
+
 //# sourceMappingURL=index.js.map
