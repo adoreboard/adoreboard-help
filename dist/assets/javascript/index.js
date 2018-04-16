9f148f829117e3276755565c46dbfd32594d98c9
Michael Mckeever <mckeever02@gmail.com>
2018-04-16 13:30:53 +0100

    publish dist folder

---
+++ dist/assets/javascript/index.js
@@ -0,0 +1,17 @@
+
+// var sticky = new Sticky('.sticky');
+
+var elm = document.querySelector('#post-menu');
+var ms = new MenuSpy(elm,{
+  threshold: 15
+});
+
+stickybits('#sticky', {useStickyClasses: true});
+
+var options = {
+    valueNames: [ 'post-title', 'post-list-item']
+};
+
+var spacesList = new List('help', options);
+
+//# sourceMappingURL=index.js.map
